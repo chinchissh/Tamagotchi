73f59df0899578a335aadc41deb046ff108071e8
Sany-SHiriCE <Sany-SHiriCE@302D-10>
2023-05-04 15:26:25 +0700

    Добавьте файлы проекта.

---
+++ Tamagotchi/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Tamagotchi//Лабораторная работа по Мищенко №2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Tamagotchi tama = new Tamagotchi(5, 5, 5);
+            bool gameOver = false;
+            while (!gameOver)
+            {
+                Console.WriteLine("Что бы вы хотели сделать?\n");
+                Console.WriteLine("1. Покормить Тамагочи");
+                Console.WriteLine("2. Поиграть с Тамагочи");
+                Console.WriteLine("3. Уложить Тамагочи спать");
+                Console.WriteLine("4. Узнать состояние Тамагочи");
+                Console.WriteLine("5. Оставить своего питомца :c");
+
+                int choice = int.Parse(Console.ReadLine());
+
+                switch (choice)
+                {
+                    case 1:
+                        tama.Feed();
+                        break;
+                    case 2:
+                        tama.Play();
+                        break;
+                    case 3:
+                        tama.Sleep();
+                        break;
+                    case 4:
+                        tama.CheckStatus();
+                        break;
+                    case 5:
+                        Console.WriteLine("Спасибо за игру");
+                        gameOver = true;
+                        break;
+                    default:
+                        Console.WriteLine("Неверная команда, попробуйте ещё раз.");
+                        break;
+                }
+            }
+        }
+    }
+}
+
---
+++ Tamagotchi/Tamagotchi.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tamagotchi
+{
+    class Tamagotchi
+    {
+        private int hunger;
+        private int happiness;
+        private int energy;
+        public Tamagotchi(int hunger, int happiness, int energy)
+        {
+            this.hunger = hunger;
+            this.happiness = happiness;
+            this.energy = energy;
+        }
+
+        public void Feed()
+        {
+            Console.WriteLine("Кормим тамагочи...");
+            hunger--;
+            happiness++;
+        }
+
+        public void Play()
+        {
+            Console.WriteLine("Играем с тамагочи...");
+            happiness++;
+            energy--;
+        }
+
+        public void Sleep()
+        {
+            Console.WriteLine("Укладываем тамагочи спать...");
+            energy++;
+            hunger++;
+        }
+
+        public void CheckStatus()
+        {
+            Console.WriteLine("Состояние Тамагочи:");
+            Console.WriteLine("Голод: " + hunger);
+            Console.WriteLine("Счастье: " + happiness);
+            Console.WriteLine("Энергия: " + energy);
+        }
+    }
+}
